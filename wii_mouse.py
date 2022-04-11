97c814bb207b816fa23ecf9d0b8456c07daa0313
Wiener234 <wiemernils@gmail.com>
2022-04-11 22:52:35 +0200

    add left click

--- wii_mouse.py
+++ wii_mouse.py
@@ -41,17 +41,25 @@ wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_IR
 #turn on led to show connected 
 wm.led = 1
 
-xy = {'x': 0, 'y': 0, 'exit': 0}
+xy = {'x': 0, 'y': 0, 'exit': 0, 'b': 0}
 
 while True:
     button = wm.state['buttons']
-    if button == 8:
+    if button == 3:
         xy['exit'] = 1
         c.send(pickle.dumps(xy))
         s.close()
         exit(wm)
         quit()
-    time.sleep(0.01)
+    
+    time.sleep(0.03)
+
+    if button == 4:
+      xy['b'] = 1
+      c.send(pickle.dumps(xy))
+    else:
+      xy['b'] = 0
+      c.send(pickle.dumps(xy))
 
     #error when pos none
     if wm.state['ir_src'][0] != None and wm.state['ir_src'][1] != None:
@@ -71,7 +79,10 @@ while True:
         # y is overshooting 1080 on monitor
         y = (y1+y2)
 
-        xy = {'x': x, 'y': y, 'exit': 0}
+        xy['x'] = x
+        xy['y'] = y
+
+        #xy = {'x': x, 'y': y, 'exit': 0}
 
         c.send(pickle.dumps(xy))
 
