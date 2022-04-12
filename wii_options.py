c245f05d95e425ceee04e85ddd7a59373bdfcce1
Wiener234 <wiemernils@gmail.com>
2022-04-12 21:38:46 +0200

    option to choose device and add gyro for motioplus

--- wii_options.py
+++ wii_options.py
@@ -24,62 +24,99 @@ wm = None
 bal = None
 i=2
 
-# while not wm: 
-#   print("Press 1+2 now to connect...")
-#   try:
-#     wm=cwiid.Wiimote("00:25:A0:D1:B2:F4")
-#     wm.rumble = 1
-#     wm.led = 1
-#     sleep(0.5)
-#     wm.rumble = 0
-#     print("Wii-Mote Connected...")
-#   except RuntimeError: 
-#     if (i>10): 
-#       quit() 
-#       break 
-#     print ("Error opening wiimote connection" )
-#     print ("attempt " + str(i) )
-#     i +=1
+
+
+if sys.argv[1] == 'wiimote' or sys.argv[1] == 'both':
+  while not wm: 
+    print("Press 1+2 now to connect...")
+    try:
+      wm=cwiid.Wiimote("00:25:A0:D1:B2:F4")
+      # wm.enable(cwiid.FLAG_MESG_IFC | cwiid.FLAG_MOTIONPLUS)
+      wm.enable(cwiid.FLAG_MOTIONPLUS)
+      wm.rpt_mode = cwiid.RPT_MOTIONPLUS | cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_IR | cwiid.RPT_NUNCHUK | cwiid.RPT_BALANCE
+      wm.rumble = 1
+      wm.led = 1
+      sleep(0.5)
+      wm.rumble = 0
+      print("Wiimote Connected...")
+    except RuntimeError: 
+      if (i>10): 
+        quit() 
+        break 
+      print ("Error opening wiimote connection" )
+      print ("attempt " + str(i) )
+      i +=1
 
 
 
 
+if sys.argv[1] == 'balance' or sys.argv[1] == 'both':
+  while not bal:
+    print("Press the red button now to connect...")
+    sleep(2)
+    try:  
+      bal = cwiid.Wiimote("78:A2:A0:25:8E:5D")
+      bal.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_BALANCE | cwiid.RPT_NUNCHUK
+      print("Board Connected...")
+    except RuntimeError: 
+      if (i>10): 
+        quit() 
+        break 
+      print ("Error opening wiimote connection" )
+      print ("attempt " + str(i) )
+      i +=1 
 
-while not bal:
-  print("Press the red button now to connect...")
-  sleep(2)
-  try:  
-    bal = cwiid.Wiimote("78:A2:A0:25:8E:5D")
-    print("Board Connected...")
-  except RuntimeError: 
-    if (i>10): 
-      quit() 
-      break 
-    print ("Error opening wiimote connection" )
-    print ("attempt " + str(i) )
-    i +=1 
-
-bal.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_BALANCE | cwiid.RPT_NUNCHUK
-#wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_IR | cwiid.RPT_NUNCHUK | cwiid.RPT_BALANCE
 
 #print('Battery: ' + str(wm.state['battery']))
 
-while True:
-    sleep(2)
+if sys.argv[1] == 'wiimote':
+  while True:
+      sleep(0.5)
 
-    #buttons = wm.state['buttons']
-    buttons_bal = bal.state['buttons']
-
-    # if buttons == btn_1 + btn_2:
-    #     exit()
-
-    if buttons_bal == 8:
-      exit()
+      buttons = wm.state['buttons']
 
 
-    # if wm.state['ir_src'][0] != None:
-    #     print(type(wm.state['ir_src'][0]['size']))
-    print(bal.state)
+      if buttons == btn_1 + btn_2:
+         exit()
+        
+      print(wm.state)
+
+
+
+if sys.argv[1] == 'balance':
+  while True:
+      #sleep(2)
+  
+
+      buttons_bal = bal.state['buttons']
+
+      if buttons_bal == 8:
+        exit()
+      try:
+        print(bal.state['balance'])
+      except:
+        pass
+  
+
+
+if sys.argv[1] == 'both':
+  while True:
+      sleep(2)
+  
+      buttons = wm.state['buttons']
+      buttons_bal = bal.state['buttons']
+  
+      if buttons == btn_1 + btn_2:
+          exit()
+  
+      if buttons_bal == 8:
+        exit()
+  
+
+
+
+
+
 
 
 
@@ -103,3 +140,6 @@ while True:
 #wm.state['nunchuk']['buttons'] = int
 
 
+# acc = 1 tilt left an rigth | 2 tilt for and back | 3 on top or not
+
+# angle and stuff https://stackoverflow.com/questions/63939618/pitch-and-yaw-from-a-wiimote-motion-plus-using-cwiid-python
